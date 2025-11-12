13478e6bd9ddd32242e03810d809c45e91e94b6e
ghazalababar16 <g.babar.khan@gmail.com>
2025-11-13 01:52:26 +0500

    done

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 
 
+
 let addTask = document.getElementById('addTask');
 let task = document.getElementById('task');
 let list = document.getElementById('list');
 
 addTask.addEventListener('click', () => {
+    if (addTask.innerHTML == "Add Task") {
 
-    list.innerHTML += `<div class= "main"> <li>${task.value}</li> 
-           <button id = "btn" onclick ="btn(this)" >Edit</button>
-            <button id = "del" >delete</button></div>`
+        list.innerHTML += `<div class= "main"> <li>${task.value}</li> <div> <button id = "edit" onclick ="edit(this)" >Edit</button>
+            <button id = "del" >Delete</button></div></div>`
 
-    task.value = '';
+        task.value = '';
+    }
+    else {
+        let updatedTodo = task.value;
+        let element = document.querySelector(".main").firstElementChild;
+       
+        
+        let  userTodo = element.innerHTML;
+        
+       userTodo = task.value;
+        // userTodo.innerText = updatedTodo
+
+    }
 })
 
-function btn(element) {
-    let btn = document.getElementById("btn")
+function edit(element) {
     element.addEventListener("click", () => {
-        console.log(element.parentNode.children);
 
+        task.value = element.parentNode.parentNode.firstElementChild.innerText;
+
+        addTask.innerHTML = "save"
     })
 
 }
